2b492b94a658e8e8c87bc7f7aae0aa23d15b4409
Hatzen <kaihartz@gmx.net>
2022-01-05 21:25:32 +0100

    dev state show valids vs invalid

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import entryStore, { EntryStore } from '../stores/entryStore'
 import BoxPlot from './boxplot'
+import BarPlot from './barplot'
 import { Allotment, AllotmentHandle } from 'allotment'
 import 'allotment/dist/style.css'
 import ControlPane from './controlPane'
@@ -8,15 +9,22 @@ import DisclaimerModal from './disclaimerModal'
 import MenuAppBar from './appBar'
 import { Provider } from 'mobx-react'
 import controlStore, { ControlStore } from '../stores/controlStore'
+import { Box, Tab, Tabs } from '@material-ui/core'
 
-class App extends React.Component {
+interface AppState {
+    components: number[],
+    usePlot: number
+}
+
+class App extends React.Component<any, AppState> {
     private controlPane: React.RefObject<AllotmentHandle>
     
     constructor(props: any) {
         super(props)
         this.controlPane = React.createRef()
         this.state ={
-            components: [0, 1]
+            components: [0, 1],
+            usePlot: 0
         }
     }
 
@@ -33,14 +41,25 @@ class App extends React.Component {
             <div style={fitAll}>
                 <Provider {...stores}>
                     <DisclaimerModal fullScreen={false}></DisclaimerModal>
-                    <MenuAppBar menuClicked={this.toggleControls.bind(this)}></MenuAppBar>
+                    <MenuAppBar menuClicked={this.toggleControls.bind(this)}>
+                    </MenuAppBar>
                     <div style={{position: 'absolute', top: 64, bottom: 0, left: 0, right:0}}>
                         <Allotment ref={this.controlPane}>
                             {panes.map((pane: number) => {
                                 if (pane === 0) {
                                     return (
                                         <Allotment.Pane key={pane}>
-                                            <BoxPlot></BoxPlot>
+                                            <div style={{position: 'relative', top: 0, left: 0, right: 0}} >
+                                                <Tabs
+                                                    value={''}
+                                                    onChange={this.changePlot.bind(this)}>
+                                                    <Tab label="Salary" />
+                                                    <Tab label="Participation" />
+                                                </Tabs>
+                                            </div>
+                                            <div style={{position: 'relative', top: 0, left: 0, right: 0, height: 'calc(100% - 48px)'}}>
+                                                {this.state.usePlot === 0 ? <BoxPlot></BoxPlot> : <BarPlot></BarPlot>}
+                                            </div>
                                         </Allotment.Pane>
                                     )
                                 } else {
@@ -56,6 +75,16 @@ class App extends React.Component {
                 </Provider>
             </div>
         )
+    }
+
+    private changePlot(): void {
+        let showPlot: number
+        if (this.state.usePlot === 1) {
+            showPlot = 0
+        } else {
+            showPlot = 1
+        }
+        this.setState({usePlot: showPlot})
     }
 
     private toggleControls(): void {
---
+++ src/components/barplot.tsx
@@ -0,0 +1,64 @@
+import React from 'react'
+import Plot from 'react-plotly.js'
+import { inject, observer } from 'mobx-react'
+import { StoreProps } from './app'
+
+class BarPlot extends React.Component<StoreProps> {
+
+    render(): JSX.Element {
+        return (
+            <div style={{background: 'rgba(52, 52, 52, 0.8)', zIndex:1000, padding: 'auto',
+                position: 'absolute', top: 0, left: 0, right:0, bottom: 0}}>
+                <div style={{position: 'absolute', top: 0, bottom: 0, left:0, right: 0, overflow: 'auto'}}>
+                    <Plot
+                        data={this.data}
+                        layout={ {barmode: 'group', width: this.width, height: this.height, title: '', showlegend: false} }
+                    // TODO: Check Layout.template
+                    // TODO: Check Config.static for temporary disable?
+                    />
+                </div>
+            </div>
+        )
+    }
+
+    private get data(): any { // TODO: Plotty Data
+        const resultList = this.props.entryStore!.parsedDataByYear
+        
+        const displayYears = this.props.controlStore?.controlState.selectedYears
+
+        const invalidNumbers = Object.keys(resultList)
+            .filter(year => displayYears![year as any] === true)
+            .map(key => resultList[key as any].invalidEntryCount)
+
+        const overallNumbers = Object.keys(resultList)
+            .filter(year => displayYears![year as any] === true)
+            .map(key => resultList[key as any].overallEntryCount)
+
+
+        const trace1 = {
+            x: displayYears,
+            y: overallNumbers,
+            name: 'allParticipations',
+            type: 'bar'
+        }
+      
+        const trace2 = {
+            x: displayYears,
+            y: invalidNumbers,
+            name: 'considered invalid',
+            type: 'bar'
+        }
+      
+        return [trace1, trace2]
+    }
+
+    get width(): number {
+        return window.innerWidth * 0.8
+    }
+    
+    get height(): number {
+        return window.document.documentElement.clientHeight
+    }
+}
+
+export default inject('entryStore', 'controlStore')(observer(BarPlot))
